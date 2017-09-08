48ae46485894273177a0550e7725fe542b966640
Mark Seda <mseda@udel.edu>
2017-09-08 11:33:23 -0400

    Working! Looks pretty good as far as functionality. Style tho...
    Code was brute force in places, we may want to fix it some more?...
    Wondering if we will have to work on this code more next week, if so, it
    should be more modular. (Enums and Loops vs hardcoded integers and
    repetitive calls)

--- Lab2/src/Animation.java
+++ Lab2/src/Animation.java
@@ -6,28 +6,41 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Animation extends JPanel {
-
-    final int frameCount = 10;
     int picNum = 0;
-    BufferedImage[] pics;
+    BufferedImage[] currentPics;
+    
+    ArrayList<BufferedImage[]> allPics = new ArrayList<BufferedImage[]>(); //ArrayList of arrays of buffered images
+    
     int xloc = 0;
     int yloc = 0;
     final int xIncr = 20;
     final int yIncr = 20;
+    
+    // The below are only constant because the forward_ animations have the same characteristics. 
+    final static int frameCount = 10;
     final static int frameWidth = 500;
     final static int frameHeight = 300;
     final static int imgWidth = 165;
     final static int imgHeight = 165;
+    
     public enum OrcAction {
-    	FORWARD_NORTH,FORWARD_SOUTH,FORWARD_EAST,FORWARD_WEST, FORWARD_NORTHEAST, FORWARD_NORTHWEST, FORWARD_SOUTHEAST, FORWARD_SOUTHWEST
-    	}
-    OrcAction currentAction = OrcAction.FORWARD_SOUTHWEST; //initialized in constructor, specifies which images to display and which direction to move.
+    	FORWARD_NORTH,FORWARD_SOUTH,FORWARD_EAST,FORWARD_WEST, FORWARD_NORTHEAST, FORWARD_NORTHWEST, FORWARD_SOUTHEAST, FORWARD_SOUTHWEST;
+    }
+    
+    
+    
+    
+    OrcAction currentAction = OrcAction.FORWARD_SOUTHEAST; //initialized in constructor, specifies which images to display and which direction to move.
    
+    boolean actionHasChanged = false;
+    
 
     //Override this JPanel's paint method to cycle through picture array and draw images
     public void paint(Graphics g) {
@@ -39,7 +52,9 @@ public class Animation extends JPanel {
     	 */
    
     	if(xloc >= (frameWidth - imgWidth)){// East boundary switching direction
-    		g.drawImage(pics[picNum], xloc, yloc, Color.gray, this);
+    		g.drawImage(currentPics[picNum], xloc, yloc, Color.gray, this);
+    		
+    		actionHasChanged = true;
     		
     		switch(currentAction){// Redirection
 	    		case FORWARD_EAST:
@@ -54,7 +69,10 @@ public class Animation extends JPanel {
     		}
     	}
     	else if (xloc < 0){//West boundary switching direction
-    		g.drawImage(pics[picNum], xloc, yloc, Color.gray, this);
+    		g.drawImage(currentPics[picNum], xloc, yloc, Color.gray, this);
+    		
+    		actionHasChanged = true;
+    		
     		switch(currentAction){
 	    		case FORWARD_WEST:
 	    			currentAction = OrcAction.FORWARD_EAST;
@@ -68,7 +86,9 @@ public class Animation extends JPanel {
 	    	}
     	}
     	else if (yloc >= (frameHeight - imgHeight)){// South boundary switching direction
-    		g.drawImage(pics[picNum], xloc, yloc, Color.gray, this);
+    		g.drawImage(currentPics[picNum], xloc, yloc, Color.gray, this);
+    		actionHasChanged = true;
+    		
     		switch(currentAction){
 	    		case FORWARD_SOUTH:
 	    			currentAction = OrcAction.FORWARD_NORTH;
@@ -82,7 +102,8 @@ public class Animation extends JPanel {
 	    	}
     	}
     	else if (yloc < 0){// North boundary switching direction
-    		g.drawImage(pics[picNum], xloc, yloc, Color.gray, this);
+    		g.drawImage(currentPics[picNum], xloc, yloc, Color.gray, this);
+    		actionHasChanged = true;
     		switch(currentAction){
 	    		case FORWARD_NORTH:
 	    			currentAction = OrcAction.FORWARD_SOUTH;
@@ -99,28 +120,28 @@ public class Animation extends JPanel {
     	
     	 switch(currentAction){// Increments coordinates by direction
 	 		case FORWARD_NORTH:
-	 			g.drawImage(pics[picNum], xloc, yloc-=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc, yloc-=yIncr, Color.gray, this);
 	 			break;
 	 		case FORWARD_SOUTH:
-	 			g.drawImage(pics[picNum], xloc, yloc+=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc, yloc+=yIncr, Color.gray, this);
 	 			break;
 	 		case FORWARD_EAST:
-	 			g.drawImage(pics[picNum], xloc+=xIncr, yloc, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc+=xIncr, yloc, Color.gray, this);
 	 			break;
 	 		case FORWARD_WEST:
-	 			g.drawImage(pics[picNum], xloc-=xIncr, yloc, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc-=xIncr, yloc, Color.gray, this);
 	 			break;
 	 		case FORWARD_NORTHEAST:
-	 			g.drawImage(pics[picNum], xloc+=xIncr, yloc-=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc+=xIncr, yloc-=yIncr, Color.gray, this);
 	 			break;
 	 		case FORWARD_NORTHWEST:
-	 			g.drawImage(pics[picNum], xloc-=xIncr, yloc-=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc-=xIncr, yloc-=yIncr, Color.gray, this);
 	 			break;
 	 		case FORWARD_SOUTHEAST:
-	 			g.drawImage(pics[picNum], xloc+=xIncr, yloc+=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc+=xIncr, yloc+=yIncr, Color.gray, this);
 	 			break;
 	 		case FORWARD_SOUTHWEST:
-	 			g.drawImage(pics[picNum], xloc-=xIncr, yloc+=yIncr, Color.gray, this);
+	 			g.drawImage(currentPics[picNum], xloc-=xIncr, yloc+=yIncr, Color.gray, this);
 	 			break;
 	 	}
     }
@@ -132,14 +153,17 @@ public class Animation extends JPanel {
 
     //Make frame, loop on repaint and wait
     public static void main(String[] args) {
+    	Animation Orc = new Animation();
     	JFrame frame = new JFrame();
-    	frame.getContentPane().add(new Animation());
+    	frame.getContentPane().add(Orc);
     	frame.setBackground(Color.gray);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	frame.setSize(frameWidth, frameHeight);
     	frame.setVisible(true);
     	for(int i = 0; i < 1000; i++){
     		frame.repaint();
+    		if(Orc.actionHasChanged)
+    			Orc.changeAnimation(Orc.currentAction);
     		try {
     			Thread.sleep(100);
     		} catch (InterruptedException e) {
@@ -150,12 +174,70 @@ public class Animation extends JPanel {
 
     //Constructor: get image, segment and store in array
     public Animation(){
-    	//this.currentAction =  OrcAction;
+        	
+    	BufferedImageGenerator forward_north = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_north.png");
+    	BufferedImageGenerator forward_south = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_south.png");
+    	BufferedImageGenerator forward_east = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_east.png");
+    	BufferedImageGenerator forward_west = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_west.png");
+    	BufferedImageGenerator forward_northeast = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_northeast.png");
+    	BufferedImageGenerator forward_northwest = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_northwest.png");
     	BufferedImageGenerator forward_southeast = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_southeast.png");
+    	BufferedImageGenerator forward_southwest = new BufferedImageGenerator(imgWidth, imgHeight, frameCount, "images/orc/orc_forward_southwest.png");
     	
-    	pics = forward_southeast.getBufferedImageArray();
+    	allPics.add(forward_north.getBufferedImageArray());
+    	allPics.add(forward_south.getBufferedImageArray());
+    	allPics.add(forward_east.getBufferedImageArray());
+    	allPics.add(forward_west.getBufferedImageArray());
+    	allPics.add(forward_northeast.getBufferedImageArray());
+    	allPics.add(forward_northwest.getBufferedImageArray());
+    	allPics.add(forward_southeast.getBufferedImageArray());
+    	allPics.add(forward_southwest.getBufferedImageArray());
     	
-    	// TODO: Change this constructor so that at least eight orc animation pngs are loaded
+    	changeAnimation(this.currentAction);
+    	
     }  
     
+    
+    public void changeAnimation(OrcAction newAction){
+    	switch(newAction){
+    	case FORWARD_NORTH:
+    		currentPics = allPics.get(0); //change to north pics
+    		actionHasChanged = false;
+    		break;
+    	case FORWARD_SOUTH:
+    		currentPics = allPics.get(1); //change to south pics
+    		actionHasChanged = false;
+    		break;	
+    	case FORWARD_EAST:
+    		currentPics = allPics.get(2); //change to north pics
+    		actionHasChanged = false;
+    		break;
+    	case FORWARD_WEST:
+    		currentPics = allPics.get(3); //change to south pics
+    		actionHasChanged = false;
+    		break;	
+    	case FORWARD_NORTHEAST:
+    		currentPics = allPics.get(4); //change to north pics
+    		actionHasChanged = false;
+    		break;
+    	case FORWARD_NORTHWEST:
+    		currentPics = allPics.get(5); //change to south pics
+    		actionHasChanged = false;
+    		break;	
+    	case FORWARD_SOUTHEAST:
+    		currentPics = allPics.get(6); //change to north pics
+    		actionHasChanged = false;
+    		break;
+    	case FORWARD_SOUTHWEST:
+    		currentPics = allPics.get(7); //change to south pics
+    		actionHasChanged = false;
+    		break;	
+    	default:
+    		break;			
+    	}
+    	actionHasChanged = false;
+    }
+    	
 }
+    
+
